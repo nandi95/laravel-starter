803ca72cac02c7f5d4a8063ca035152d30bc988d
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2024-11-29 08:55:48 +0000

    add cast and jobs

---
+++ app/Casts/Monetary.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Database\Eloquent\Model;
+
+class Monetary implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    #[\Override]
+    public function get(Model $model, string $key, mixed $value, array $attributes): float
+    {
+        return (int) $value / 100;
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    #[\Override]
+    public function set(Model $model, string $key, mixed $value, array $attributes): int
+    {
+        return (int) $value * 100;
+    }
+}
---
+++ app/Jobs/StageUnverifiedUsersForDeletion.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use App\Notifications\UserHasNoVerification;
+use Illuminate\Console\Command;
+
+class StageUnverifiedUsersForDeletion extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:stage-unverified-users-for-deletion';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Users who did not verify their email within a certain time frame are staged for deletion';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        User::whereNull('email_verified_at')
+            ->where('created_at', '<=', now()->subDays(User::VERIFICATION_DELAY))
+            ->each(function (User $user): void {
+                $user->notify(new UserHasNoVerification);
+                $user->delete();
+            });
+    }
+}
